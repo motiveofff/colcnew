04cfdc025b0251d46ca5afe37b12758dd13d04ee
motiveofff <mintivetv@gmail.com>
2025-06-17 16:50:01 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/colc/', // имя репозитория
+  base: '/colcnew/', // имя репозитория
   optimizeDeps: {
     exclude: ['lucide-react']
   }
